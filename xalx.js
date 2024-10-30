f435e448c7b0bb3ddf9e5962bc2fc91d7b539be9
ThisumM05 <thisummadappuli@gmail>
2024-10-30 11:35:17 +0400

    first commit

---
+++ xalx.js
@@ -0,0 +1,28 @@
+
+function calculate(operation) {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    let result;
+
+    if (isNaN(num1) || isNaN(num2)) {
+        console.log("The number you have entered is not valid.");
+        result = "The number you have entered is not valid.";
+    } else {
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num2 !== 0 ? num1 / num2 : "Cannot divide by zero";
+                break;
+        }
+    }
+
+    document.getElementById('result').innerText = result;
+}
